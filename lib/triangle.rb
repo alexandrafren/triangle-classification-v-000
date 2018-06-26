99cbba68a47cabb60e599b11dd05283a667ae4ee
Alexandra Fren <alexandra.fren@gmail.com>
2018-06-26 18:49:19 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,19 +15,19 @@ class Triangle
   #    rescue TriangleError => error
   #      puts error.message
   #    end
-  #  elsif @side_1 == 0 || @side_2 == 0 || @side_3 == 0
-    #  begin
-    #   raise TriangleError
-    #  rescue TriangleError => error
-  #      puts error.message
-  #    end
+    if @side_1 <= 0 || @side_2 <= 0 || @side_3 <= 0
+      begin
+       raise TriangleError
+     rescue TriangleError => error
+       puts error.message
+     end
   #elsif @side_1 + @side_2 < @side_3 || @side_1 + @side_3 < @side_2 || @side_2 + @side_3 < @side_1
   #  begin
   #     raise TriangleError
   #    rescue TriangleError => error
   #      puts error.message
 #      end
-   if @side_1 == @side_2
+   elsif @side_1 == @side_2
      if @side_2 != @side_3
        @type = :isosceles
      else
