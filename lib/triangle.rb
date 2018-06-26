c94364b0d644c58ab9c05b6a5ae7e944e7bb864f
Alexandra Fren <alexandra.fren@gmail.com>
2018-06-26 18:51:48 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,7 +1,7 @@
 class Triangle
   attr_reader :side_1, :side_2, :side_3
 
-  def initialize(side_1, side_12, side_3)
+  def initialize(side_1, side_2, side_3)
     @side_1 = side_1
     @side_2 = side_2
     @side_3 = side_3
