c1f6d3ba0c694cbd89d3bcf7d5cfec132fbbda84
Alexandra Fren <alexandra.fren@gmail.com>
2018-06-26 18:32:48 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,19 +14,19 @@ class Triangle
       rescue TriangleError => error
         puts error.message
       end
-    elsif @side_1 == 0 || @side_2 == 0 || @side_3 == 0
-      begin
-        raise TriangleError
-      rescue TriangleError => error
-        puts error.message
-      end
-    elsif @side_1 + @side_2 < @side_3 || @side_1 + @side_3 < @side_2 || @side_2 + @side_3 < @side_1
-      begin
-        raise TriangleError
-      rescue TriangleError => error
-        puts error.message
-      end
-    elsif @side_1 == @side_2 && @side_2 == @side_3
+    #elsif @side_1 == 0 || @side_2 == 0 || @side_3 == 0
+    #  begin
+    #    raise TriangleError
+    #  rescue TriangleError => error
+    #    puts error.message
+  #    end
+  #  elsif @side_1 + @side_2 < @side_3 || @side_1 + @side_3 < @side_2 || @side_2 + @side_3 < @side_1
+  #  begin
+  #      raise TriangleError
+  #    rescue TriangleError => error
+  #      puts error.message
+  #    end
+   elsif @side_1 == @side_2 && @side_2 == @side_3
       @type = :equilateral
     elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3
       @type = :isosceles
