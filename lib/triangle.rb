667c605005c6237450dbb1d5a64983895b87afe0
Alexandra Fren <alexandra.fren@gmail.com>
2018-06-26 19:00:45 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,6 @@ class Triangle
   def kind
     if (@side_1 || @side_2 || @side_3) <= 0
         raise TriangleError
-      end
     elsif @side_1 == @side_2 && @side_2 == @side_3
       :equilateral
     elsif @side_1 == @side_2 || @side_1 == @side_3 || @side_2 == @side_3
