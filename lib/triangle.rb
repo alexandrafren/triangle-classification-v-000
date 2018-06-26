28817cb85a29183301ffc3d0cd732f629beb440c
Alexandra Fren <alexandra.fren@gmail.com>
2018-06-26 18:46:13 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -32,6 +32,7 @@ class Triangle
        @type = :equilateral
      else
        @type = :isosceles
+     end
     elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3
       @type = :isosceles
     else
