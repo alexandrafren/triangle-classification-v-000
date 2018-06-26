6f7842626b05b9f6eb79b2c5b6ee5b24e1cf875c
Alexandra Fren <alexandra.fren@gmail.com>
2018-06-26 18:44:41 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -27,7 +27,7 @@ class Triangle
   #    rescue TriangleError => error
   #      puts error.message
 #      end
-   elsif @side_1 == @side_2 && @side_2 == @side_3
+   if @side_1 == @side_2 && @side_2 == @side_3
      @type = :equilateral
     elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3
       @type = :isosceles
