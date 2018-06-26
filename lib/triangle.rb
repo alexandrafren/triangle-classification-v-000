7632dcfd5a0aef4f9b8404eb02175e2ea5d4860c
Alexandra Fren <alexandra.fren@gmail.com>
2018-06-26 18:44:29 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,24 +9,24 @@ class Triangle
   end
 
   def kind
-    if @side_1.class != Fixnum || @side_2.class != Fixnum || @side_3.class != Fixnum
-      begin
-        raise TriangleError
-      rescue TriangleError => error
-        puts error.message
-      end
-    elsif @side_1 == 0 || @side_2 == 0 || @side_3 == 0
-      begin
-       raise TriangleError
-      rescue TriangleError => error
-        puts error.message
-      end
-  elsif @side_1 + @side_2 < @side_3 || @side_1 + @side_3 < @side_2 || @side_2 + @side_3 < @side_1
-    begin
-       raise TriangleError
-      rescue TriangleError => error
-        puts error.message
-      end
+  #  if @side_1.class != Fixnum || @side_2.class != Fixnum || @side_3.class != Fixnum
+    #  begin
+    #    raise TriangleError
+  #    rescue TriangleError => error
+  #      puts error.message
+  #    end
+  #  elsif @side_1 == 0 || @side_2 == 0 || @side_3 == 0
+    #  begin
+    #   raise TriangleError
+    #  rescue TriangleError => error
+  #      puts error.message
+  #    end
+  #elsif @side_1 + @side_2 < @side_3 || @side_1 + @side_3 < @side_2 || @side_2 + @side_3 < @side_1
+  #  begin
+  #     raise TriangleError
+  #    rescue TriangleError => error
+  #      puts error.message
+#      end
    elsif @side_1 == @side_2 && @side_2 == @side_3
      @type = :equilateral
     elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3
